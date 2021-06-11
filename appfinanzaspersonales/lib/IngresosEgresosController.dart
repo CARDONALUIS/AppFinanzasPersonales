d111dfa72d94a233019968c0a6f3ce692ee1bd9c
JAlejandroRP <jesus.98_rdz@hotmail.com>
2021-06-10 19:28:49 -0500

    Pantalla Historiales implementada

--- appfinanzaspersonales/lib/IngresosEgresosController.dart
+++ appfinanzaspersonales/lib/IngresosEgresosController.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
 Future<double> calculaPresupuesto(String uid) async {
   double presupuesto = 0.0;
@@ -26,4 +28,47 @@ Future<double> obtenTotalMovimientos(String uid, String movimiento) async {
   } catch (Exception) {
     return null;
   }
+}
+
+Future<List<movimiento>> historiales(String uid) async {
+  List<movimiento> movimientos = [];
+  movimiento m = movimiento();
+  final CollectionReference coleccion =
+      FirebaseFirestore.instance.collection("movimientos");
+
+  try {
+    await coleccion.doc(uid).collection('ingresos').get().then((ss) {
+      ss.docs.forEach((element) {
+        m = movimiento();
+        m.cantidad = element.data()['cantidad'];
+        m.tipo = element.data()['tipo'];
+        m.fecha = element.data()['fecha'];
+        m.tipoMovimiento = "Ingreso";
+        movimientos.add(m);
+      });
+    });
+
+    await coleccion.doc(uid).collection('egresos').get().then((ss) {
+      ss.docs.forEach((element) {
+        m = movimiento();
+        m.cantidad = element.data()['cantidad'];
+        m.tipo = element.data()['tipo'];
+        m.fecha = element.data()['fecha'];
+        m.tipoMovimiento = "Egreso";
+        movimientos.add(m);
+      });
+    });
+
+    return movimientos;
+  } catch (Exception) {
+    return null;
+  }
+}
+
+// ignore: camel_case_types
+class movimiento {
+  String tipoMovimiento = "";
+  String tipo = "";
+  String cantidad = "";
+  String fecha = "";
 }
